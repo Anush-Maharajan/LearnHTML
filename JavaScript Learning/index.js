7b3bff53ac14311757770d0d6bc4520da45d7420
Anush Maharjan <anushmrzan777@gmail.com>
2025-02-11 10:57:50 +0545

    javascript

--- JavaScript Learning/index.js
+++ JavaScript Learning/index.js
@@ -2,3 +2,21 @@ document.getElementById("thisBtn").onclick = function() {
     let username = prompt("Give me your money");
     console.log(username);
 }
+
+document.getElementById("Prpt").onclick = function() {
+    let username = prompt("Enter your name: ");
+    document.write("Your name is " ,username);
+}
+
+document.getElementById("multi").onclick = function() {
+    let username = prompt("What is your name?")
+    let isChoice = confirm("Are you gay?")
+
+    if(isChoice) {
+        document.write("Oil UP", username , ".......... Lap dance hann na tah xakka")
+    }
+
+    else {
+        document.write(username, ".......Lap dance hann na tah strip gardai")
+    }
+}
