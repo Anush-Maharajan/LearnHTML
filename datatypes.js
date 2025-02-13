801129dd615d0461d7d73cf83a00bc08c728dd24
Anush Maharjan <anushmrzan777@gmail.com>
2025-02-13 10:34:03 +0545

    Learning javascript

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+let str1 = "Hello";
+let str2 = "World";
+let str3 = "Welcome ${str1}!";
+console.log(str3);
+console.log("Length:", str1.length);
+console.log(str1.toUpperCase());
+
+// let num1 = ;
+
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue && isFalse);
+console.log(isTrue || isFalse);
+
+console.log("5" + 10);
+console.log("5" - 2);
+console.log(true + 1);
+console.log(false - 1);
+
+let st1 = "123";
+let num = Number(st1);
+console.log(num)
+
+let nu2 = 456;
+let st2 = String(nu2);
+console.log(st2);
+
+console.log(Boolean(0));
+console.log(Boolean(123));
+console.log(Boolean(-23));
+
